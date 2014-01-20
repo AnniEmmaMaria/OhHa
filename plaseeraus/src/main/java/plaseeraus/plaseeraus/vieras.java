97f6b1b08327891bff70270ea2e6910fa465b089
Anni <anni.jokiniemi@hotmail.com>
2014-01-20 20:06:16 +0200

    Koodin aloitus

---
+++ plaseeraus/src/main/java/plaseeraus/plaseeraus/vieras.java
@@ -0,0 +1,14 @@
+package plaseeraus.plaseeraus;
+
+public class vieras {
+    private String etunimi;
+    private String sukunimi;
+    private String sukupuoli;
+    
+    public vieras (String etunimi, String sukunimi, String sukupuoli){
+        this.etunimi = etunimi;
+        this.sukunimi = sukunimi;
+        this.sukupuoli = sukupuoli;
+    }
+    
+}
---
+++ plaseeraus/src/main/java/plaseeraus/plaseeraus/vieraslistanLukija.java
@@ -0,0 +1,20 @@
+package plaseeraus.plaseeraus;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class vieraslistanLukija {
+    //Määritetään luettava vieraslista ja sille lukija
+    File vieraslista = new File("src\\main\\java\\Vieraslistatiedosto\\vieraslista.txt");
+    Scanner listanLukija = new Scanner(vieraslista);
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+}
